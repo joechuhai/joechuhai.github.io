b602164cfa5acae10e40852cdb9eca3610116f74
joechuhai <joechuhai@users.noreply.github.com>
2025-01-04 03:28:27 +0000

    deploy: 7ec23cad1c8af90ebfaf1b7e06cb2aec0ac848d2

--- en/assets/js/data/swconf.js
+++ en/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1735960987',resources: [
+    cacheName: 'chirpy-1735961305',resources: [
       '/en/assets/css/jekyll-theme-chirpy.css',
       '/en/',
       
