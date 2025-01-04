51f622fddcf38a414b155718c24b3d3938a1cf94
joechuhai <joechuhai@users.noreply.github.com>
2025-01-04 03:17:19 +0000

    deploy: 3d2dfd8a7f92093628ba47c2ff24998baea25af6

---
+++ en/assets/js/data/swconf.js
@@ -0,0 +1,37 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1735960637',resources: [
+      '/en/assets/css/jekyll-theme-chirpy.css',
+      '/en/',
+      
+        '/en/categories/',
+      
+        '/en/tags/',
+      
+        '/en/archives/',
+      
+        '/en/about/',
+      
+
+      
+      
+        '/en/assets/js/dist/categories.min.js',
+        '/en/assets/js/dist/commons.min.js',
+        '/en/assets/js/dist/home.min.js',
+        '/en/assets/js/dist/misc.min.js',
+        '/en/assets/js/dist/page.min.js',
+        '/en/assets/js/dist/post.min.js',
+        '/en/assets/js/dist/theme.min.js'
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
---
+++ zh/assets/js/data/swconf.js
@@ -0,0 +1,29 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1735960638',resources: [
+      '/zh/assets/css/jekyll-theme-chirpy.css',
+      '/zh/',
+      
+
+      
+      
+        '/zh/assets/js/dist/categories.min.js',
+        '/zh/assets/js/dist/commons.min.js',
+        '/zh/assets/js/dist/home.min.js',
+        '/zh/assets/js/dist/misc.min.js',
+        '/zh/assets/js/dist/page.min.js',
+        '/zh/assets/js/dist/post.min.js',
+        '/zh/assets/js/dist/theme.min.js'
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
